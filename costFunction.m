c747dc0a6b92146e51ad83ca077e4e7c9705fd8d
dbolotov <dbolotov@live.com>
2012-06-23 22:38:15 -0700

    first commit

---
+++ costFunction.m
@@ -0,0 +1,16 @@
+function [J, grad] = costFunction(theta, X, y)
+%COSTFUNCTION Compute cost and gradient for logistic regression
+%   [J, grad] = COSTFUNCTION(theta, X, y) computes the cost of using theta as the
+%   parameter for logistic regression and the gradient of the cost
+%   w.r.t. to the parameters.
+%
+%	Code source: ml-class.org
+
+m = length(y); % number of training examples
+J = 0;
+grad = zeros(size(theta));
+
+J = (1/m)*(-y'*log(sigmoid(X*theta)) - (1-y)'*log(1-sigmoid(X*theta)));
+grad = ((1/m)*(sigmoid(X*theta) - y)'*X)';
+
+end
