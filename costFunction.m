d4ac24c16e47d608955a0fca3feeb39b2ae5c829
dbolotov <dbolotov@live.com>
2012-06-23 22:58:59 -0700

    cleaned up comments

--- costFunction.m
+++ costFunction.m
@@ -7,10 +7,7 @@ function [J, grad] = costFunction(theta, X, y)
 %	Code source: ml-class.org
 
 m = length(y); % number of training examples
-J = 0;
-grad = zeros(size(theta));
 
 J = (1/m)*(-y'*log(sigmoid(X*theta)) - (1-y)'*log(1-sigmoid(X*theta)));
 grad = ((1/m)*(sigmoid(X*theta) - y)'*X)';
-
 end
