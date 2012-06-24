831771fbc6b95228c3ea600b35dbaa361ab14510
dbolotov <dbolotov@live.com>
2012-06-24 00:49:29 -0700

    added confusionmat.m, sensitivity and specificity output

--- logistic_regression_script.m
+++ logistic_regression_script.m
@@ -1,6 +1,16 @@
 %Logistic Regression
+%
+% Perform logistic regression; predict output (2-class variable) using
+% unconstrained optimization routine (fminunc)
+%
+% Functions used: sigmoid.m, costFunction.m
+%
+% Code based on ml-class.org Ex.2
+% 
 
-%predicts output using fminunc
+%to do:
+%generate confusion matrix
+%display sensitivity and specificity
 
 
 %Input must contain feature columns followed by dependent variable column at end
@@ -11,11 +21,10 @@ alpha = 0.01;
 num_iters = 1000;
 
 %percentage of data to use for training
-train_perc = .85;
+train_perc = .77;
 
 %extract columns to use
 X = data(:,1:end-1);
-X_orig = X;
 y = data(:,end);
 
 %split into training and test sets:
@@ -23,15 +32,12 @@ test_rows = round(size(X,1)*(1-train_perc)); %number of rows to use in test set
 X_test = X(1:test_rows,:); y_test = y(1:test_rows,:);%this is the test set
 X = X(test_rows+1:end,:); y = y(test_rows+1:end,:);%this is the training set
 
-%  Setup the data matrix appropriately, and add ones for the intercept term
-[m, n] = size(X);
-
 %Add intercept term to X
 X = [ones(size(X,1), 1) X];
 X_test = [ones(size(X_test,1), 1) X_test];
 
 % Initialize fitting parameters
-initial_theta = zeros(n + 1, 1);
+initial_theta = zeros(size(X,2), 1);
 
 %  Set options for fminunc
 options = optimset('GradObj', 'on', 'MaxIter', 400);
@@ -46,15 +52,16 @@ fprintf('Cost at theta found by fminunc: %f\n', cost);
 fprintf('theta: \n');
 fprintf(' %f \n', theta);
 
-p_train = (sigmoid(X*theta) >= 0.5);
+p_train = double(sigmoid(X*theta) >= 0.5);
 fprintf('Training set accuracy: %f\n', mean(double(p_train == y)) * 100);
 
-p_test = (sigmoid(X_test*theta) >= 0.5);
+p_test = double(sigmoid(X_test*theta) >= 0.5);
 fprintf('Test set accuracy: %f\n', mean(double(p_test == y_test)) * 100);
 
-
-
-
+%confusion matrix
+cm = confusionmat(y_test,p_test);
+sens = cm(1,1) / (cm(1,1) + cm(1,2)); %ability to identify positive class
+spec = cm(2,2) / (cm(2,2) + cm(2,1)); %ability to identify negative class
 
 
 
