4a311242ccfeb409113ac229879fb9d5fb7b0dff
dbolotov <dbolotov@live.com>
2012-06-23 23:51:58 -0700

    added prediction steps

--- logistic_regression_script.m
+++ logistic_regression_script.m
@@ -11,7 +11,7 @@ alpha = 0.01;
 num_iters = 1000;
 
 %percentage of data to use for training
-train_perc = .95;
+train_perc = .85;
 
 %extract columns to use
 X = data(:,1:end-1);
@@ -23,30 +23,15 @@ test_rows = round(size(X,1)*(1-train_perc)); %number of rows to use in test set
 X_test = X(1:test_rows,:); y_test = y(1:test_rows,:);%this is the test set
 X = X(test_rows+1:end,:); y = y(test_rows+1:end,:);%this is the training set
 
-
-%Use training set to get regression parameters:
-
-%Compute mean and standard deviation, normalize X
-mu = mean(X); sigma = std(X);
-X = (X-repmat(mu,[size(X,1) 1]))./repmat(sigma,[size(X,1) 1]);
-
 %  Setup the data matrix appropriately, and add ones for the intercept term
 [m, n] = size(X);
 
 %Add intercept term to X
 X = [ones(size(X,1), 1) X];
+X_test = [ones(size(X_test,1), 1) X_test];
 
 % Initialize fitting parameters
 initial_theta = zeros(n + 1, 1);
-
-% Compute and display initial cost and gradient
-[cost, grad] = costFunction(initial_theta, X, y);
-
-fprintf('Cost at initial theta (zeros): %f\n', cost);
-fprintf('Gradient at initial theta (zeros): \n');
-fprintf(' %f \n', grad);
-
-
 
 %  Set options for fminunc
 options = optimset('GradObj', 'on', 'MaxIter', 400);
@@ -61,6 +46,11 @@ fprintf('Cost at theta found by fminunc: %f\n', cost);
 fprintf('theta: \n');
 fprintf(' %f \n', theta);
 
+p_train = (sigmoid(X*theta) >= 0.5);
+fprintf('Training set accuracy: %f\n', mean(double(p_train == y)) * 100);
+
+p_test = (sigmoid(X_test*theta) >= 0.5);
+fprintf('Test set accuracy: %f\n', mean(double(p_test == y_test)) * 100);
 
 
 
