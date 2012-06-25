78b4b60888f8d24e0e5c9bb8e92d203839af1a3c
dbolotov <dbolotov@live.com>
2012-06-24 18:34:21 -0700

    computes 0/1 misclassification error for 2-class prediction

---
+++ misclassError.m
@@ -0,0 +1,12 @@
+function [outp] = misclassError(inp)
+%MISCLASSERROR Compute 0/1 misclassification error for a 2-class problem
+% misclassError [out] = MISCLASSERROR(in) computes the 0/1
+% misclassification error given predicted and actual output.
+%
+%
+%
+
+
+outp = 0;
+
+end
