bc212ab89038797ac5fdd3b0adc56e9fc3dbb541
dbolotov <dbolotov@live.com>
2012-07-07 00:57:04 -0700

    finished function

--- misclassError.m
+++ misclassError.m
@@ -1,12 +1,17 @@
-function [outp] = misclassError(inp)
+function [testError] = misclassError(y,y_hat,thresh)
 %MISCLASSERROR Compute 0/1 misclassification error for a 2-class problem
 % misclassError [out] = MISCLASSERROR(in) computes the 0/1
 % misclassification error given predicted and actual output.
 %
-%
+% Input:
+% y = actual class
+% y_hat = predicted class
+% thresh = probability threshold
 %
 
+m = size(y,1);
 
-outp = 0;
+testError = (1/m) * sum(double(y_hat>=thresh & y==0 | y_hat<=thresh & y==1 ));
 
 end
+
