a2d266c10d91e12979daeb1ae0bff25f17b60174
dbolotov <dbolotov@live.com>
2012-07-09 09:57:39 -0700

    initial comit

---
+++ log_reg.py
@@ -0,0 +1,13 @@
+#!/usr/local/EPD/bin/python
+#Filename: log_reg.py
+
+#template procedure to perform logistic regression for a 2-class problem
+
+#Overview:
+#(add steps)
+
+#code ported from logistic_regression_script.m, based on ml-class.org Ex.2
+
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy import mat, c_, r_, array
